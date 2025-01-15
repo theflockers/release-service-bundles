9666883410181fdad34e660b31ae921fe58c499b
Martin Malina <mmalina@redhat.com>
2025-01-15 10:32:21 +0100

    Revert "feat(CLOUDDST-25034): run rh-sign-image-cosign signing in parallel (#701)" (#769)
    This reverts commit b820c4637ffb5c7af88cdd24f5cad311e79d8767.
    
    It seems the new version doesn't work as expected. A user reported
    issues.

--- tasks/managed/rh-sign-image-cosign/tests/pre-apply-task-hook.sh
+++ tasks/managed/rh-sign-image-cosign/tests/pre-apply-task-hook.sh
@@ -7,16 +7,14 @@ kubectl create secret generic test-cosign-secret\
   --from-literal=AWS_DEFAULT_REGION=us-test-1\
   --from-literal=AWS_ACCESS_KEY_ID=test-access-key\
   --from-literal=AWS_SECRET_ACCESS_KEY=test-secret-access-key\
-  --from-literal=SIGN_KEY=aws://arn:mykey\
-  --from-literal=PUBLIC_KEY=public_key
+  --from-literal=SIGN_KEY=aws://arn:mykey
 
 kubectl create secret generic test-cosign-secret-rekor\
   --from-literal=AWS_DEFAULT_REGION=us-test-1\
   --from-literal=AWS_ACCESS_KEY_ID=test-access-key\
   --from-literal=AWS_SECRET_ACCESS_KEY=test-secret-access-key\
   --from-literal=SIGN_KEY=aws://arn:mykey\
-  --from-literal=REKOR_URL=https://fake-rekor-server\
-  --from-literal=PUBLIC_KEY=public_key
+  --from-literal=REKOR_URL=https://fake-rekor-server
 
 # Add mocks to the beginning of task step script
 TASK_PATH="$1"
